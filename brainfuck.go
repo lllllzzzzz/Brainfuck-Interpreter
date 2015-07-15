41644ea34d6296e1b033321b980a110a41f6f629
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2015-07-16 00:17:16 +1000

    Create brainfuck.go

---
+++ brainfuck.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	//"bufio"
+	//"io"
+	"io/ioutil"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func parse(instructions []byte) {
+	var memory [30000]byte
+	var pc, p int
+
+	for ; pc < len(instructions); pc++ {
+		switch instructions[pc] {
+		case '>':
+			p++
+		case '<':
+			p--
+		case '+':
+			memory[p]++
+		case '-':
+			memory[p]--
+		case '.':
+			fmt.Printf("%c", memory[p])
+		case ',':
+			// fmt.Scanln(memory[p])
+		case '[':
+			if memory[p] == 0 {
+				loop := 1
+				for loop > 0 {
+					pc++
+					if (instructions[pc] == '[') {
+						loop++;
+					} else if (instructions[pc] == ']') {
+						loop--;
+					}
+				}
+			}
+		case ']':
+			if memory[p] != 0 {
+				loop := 1
+					for loop > 0 {
+						pc--
+						if (instructions[pc] == '[') {
+							loop--;
+						} else if (instructions[pc] == ']') {
+							loop++;
+						}
+					}
+				}
+		}
+	}
+}
+
+func main() {
+	args := os.Args
+
+	if len(args) != 2 {
+		fmt.Printf("Usage: %s filename", args[0])
+		return
+	}
+
+	filename := args[1];
+	file, err := ioutil.ReadFile(filename)
+	check(err)
+
+	//source := string(file)
+	//fmt.Println(source)
+
+	code := []byte(file)
+	parse(code)
+}
